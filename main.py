52f1e34e2e184af641c3f1109a8a046f7e38fe9c
Andrei Mishchanka <52292591+AndreiMishchanka@users.noreply.github.com>
2024-04-22 18:33:39 +0200

    small refactoring

--- main.py
+++ main.py
@@ -20,6 +20,13 @@ app.add_middleware(
 
 gm = Games_manager()
 
+@app.get("/buy/{game_id}/{player_id}")
+async def buy(game_id: int, player_id: int):
+    return await gm.buy(game_id, player_id)
+
+@app.get("/connect/{user_id}/{game_id}")
+async def connect(user_id: int, game_id: int):
+    await websockets.connect(f"ws://localhost:8000/connect/{user_id}/{game_id}")
 
 @app.get("/create/{user_id}")
 async def create(user_id: int):
@@ -27,52 +34,30 @@ async def create(user_id: int):
     print(f"Game created by user {user_id} with game_id {game_id}")
     return game_id
 
+@app.get("/end_turn/{game_id}/{player_id}")
+async def end_turn(game_id: int, player_id: int):
+    return await gm.end_turn(game_id, player_id)
 
 @app.get("/get_rule/{rule_id}")
 async def get_rule(rule_id: int):
     with open("test.json") as f:
         return json.loads(f.read())
 
-
-@app.get("/connect/{user_id}/{game_id}")
-async def connect(user_id: int, game_id: int):
-    await websockets.connect(f"ws://localhost:8000/connect/{user_id}/{game_id}")
-
-
 @app.get("/get_users/{game_id}")
 async def get_users(game_id: int):
     return await gm.get_users(game_id)
-
-
-@app.get("/buy/{game_id}/{player_id}")
-async def buy(game_id: int, player_id: int):
-    return await gm.buy(game_id, player_id)
-
-
-@app.get("/end_turn/{game_id}/{player_id}")
-async def end_turn(game_id: int, player_id: int):
-    return await gm.end_turn(game_id, player_id)
-
-
-@app.get("/roll/{game_id}/{player_id}")
-async def roll(game_id: int, player_id: int):
-    return await gm.roll(game_id, player_id)
-
-
-@app.get("/sell/{game_id}/{player_id}/{field_id}")
-async def sell(game_id: int, player_id: int, field_id: int):
-    return await gm.sell(game_id, player_id, field_id)
-
 
 @app.get("/pay/{game_id}/{player_id}")
 async def pay(game_id: int, player_id: int):
     return await gm.pay(game_id, player_id)
 
+@app.get("/roll/{game_id}/{player_id}")
+async def roll(game_id: int, player_id: int):
+    return await gm.roll(game_id, player_id)
 
-@app.get("/upgrade/{game_id}/{player_id}/{field_id}")
-async def upgrade(game_id: int, player_id: int, field_id: int):
-    return await gm.upgrade(game_id, player_id, field_id)
-
+@app.get("/sell/{game_id}/{player_id}/{field_id}")
+async def sell(game_id: int, player_id: int, field_id: int):
+    return await gm.sell(game_id, player_id, field_id)
 
 @app.get("/start_game/{game_id}/{player_id}")
 async def start_game(game_id: int, player_id: int):
@@ -81,6 +66,10 @@ async def start_game(game_id: int, player_id: int):
 @app.get("/surrender/{game_id}/{player_id}")
 async def surrender(game_id: int, player_id: int):
     return await gm.surrender(game_id, player_id)
+
+@app.get("/upgrade/{game_id}/{player_id}/{field_id}")
+async def upgrade(game_id: int, player_id: int, field_id: int):
+    return await gm.upgrade(game_id, player_id, field_id)
 
 
 # WebSockets:

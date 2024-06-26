3cfe3dd93bbcb1aa5ac1b872aefba5034b741c48
Andrei Mishchanka <52292591+AndreiMishchanka@users.noreply.github.com>
2024-06-27 00:44:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -179,7 +179,7 @@ async def keep_alive(websocket):
     while True:
         try:
             await websocket.send("ping")
-            await asyncio.sleep(30)  # Отправляем "ping" каждые 30 секунд
+            await asyncio.sleep(5)
         except websockets.exceptions.ConnectionClosed:
             full_traceback = traceback.format_exc()
             print(full_traceback)
